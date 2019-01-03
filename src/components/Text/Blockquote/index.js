2df25fe95f580d89e7887874ee41341b7f76395b
Ludwig Frank <mail@ludwigfrank.com>
2019-01-03 23:45:41 +0100

    Fix blockquote styling

--- src/components/Text/Blockquote/index.js
+++ src/components/Text/Blockquote/index.js
@@ -6,20 +6,20 @@ import { space } from 'styled-system'
 
 const Styles = styled('blockquote')`
     ${defaultStyles};
-    font-size: 24px;
+    font-size: 20px;
     margin: 0;
     > * > p {
         ${headerDefaultStyle}
-        color: ${props => props.theme.color.text.secondary};
-        font-size: 26px;
-        line-height: 34px;
+        color: ${props => props.theme.color.text.accent};
+        font-size: 24px;
+        line-height: 28px;
     }
     ${space};
 `
 
 const Blockquote = ({ children, ...props }) => {
     return (
-        <Styles {...props} mx={[0, 2, 2]} my={[2, 5, 5]}>
+        <Styles {...props} mx={[0, 2, 2]} my={[2, '40px', '40px']}>
             {children}
         </Styles>
     )
