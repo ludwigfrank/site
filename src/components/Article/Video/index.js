c39d15e228dfd39279453dbb5860feeed784c6c3
Ludwig Frank <mail@ludwigfrank.com>
2019-01-08 01:05:22 +0100

    added mobile video support

--- src/components/Article/Video/index.js
+++ src/components/Article/Video/index.js
@@ -2,24 +2,35 @@ import React from 'react'
 import { StyledVideo } from './styles'
 import ContentWrapper from '$components/Layout/ContentWrapper'
 
-const Video = ({ src, width, height, type = 'video/mp4' }) => {
-    return (
-        <ContentWrapper py={[2, 5, 5]}>
-            <StyledVideo>
-                <video
-                    autoplay
-                    loop
-                    autobuffer
-                    muted
-                    playsinline
-                    width={width ? width : 'auto'}
-                    height={height ? height : '100%'}
-                >
-                    <source src={src} type={type} />
-                </video>
-            </StyledVideo>
-        </ContentWrapper>
-    )
-}
+export default class Video extends React.Component {
+    constructor(props) {
+        super(props)
 
-export default Video
+        this.videoElement = React.createRef()
+    }
+
+    componentDidMount = () => {
+        this.videoElement.current.play()
+    }
+
+    render() {
+        const { src, width, height, type = 'video/mp4' } = this.props
+        return (
+            <ContentWrapper py={[2, 5, 5]}>
+                <StyledVideo>
+                    <video
+                        ref={this.videoElement}
+                        loop
+                        autobuffer
+                        muted
+                        playsinline
+                        width={width ? width : '100%'}
+                        height={height ? height : '100%'}
+                    >
+                        <source src={src} type={type} />
+                    </video>
+                </StyledVideo>
+            </ContentWrapper>
+        )
+    }
+}
--- src/components/Article/Video/styles.js
+++ src/components/Article/Video/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const StyledVideo = styled('div')`
     display: flex;
     justify-content: center;
+    max-width: 1056px;
     > video {
         box-shadow: ${props => props.theme.shadow[7]};
     }
